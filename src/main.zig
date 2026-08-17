480b6bcf458d83991b458a3a155d195c8490a5b8
lahatra3 <lahatra.ravelonarivo@gmail.com>
2026-08-17 23:59:31 +0300

    adding timer

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,6 @@
 const std = @import("std");
+const Io = std.Io;
+
 const Config = @import("config.zig").Config;
 const Handler = @import("handler.zig").Handler;
 
@@ -7,6 +9,8 @@ pub fn main(init: std.process.Init) !void {
     const allocator = init.arena.allocator();
     const env_map = init.environ_map;
 
+    const start_time = Io.Clock.awake.now(io);
+
     const config = try Config.load(
         allocator,
         env_map,
@@ -14,6 +18,7 @@ pub fn main(init: std.process.Init) !void {
 
     std.debug.assert(config.file_path.len > 0);
     std.debug.assert(config.pg_conn_info.len > 0);
+    std.debug.assert(config.pg_copy_query.len > 0);
 
     var handler = try Handler.init(
         io,
@@ -23,7 +28,14 @@ pub fn main(init: std.process.Init) !void {
         16,
     );
 
-    std.debug.assert(config.pg_copy_query.len > 0);
-    
+    std.log.info("start processing...", .{});
+
     try handler.run(config.pg_copy_query);
+
+    std.log.info(
+        "Duration: {} ms",
+        .{
+            start_time.untilNow(io, .awake).toMilliseconds(),
+        },
+    );
 }
